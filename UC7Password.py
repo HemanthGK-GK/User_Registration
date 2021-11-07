d19e7ca44787b1b1c60173ae350183aed788811c
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-07 21:03:33 +0530

    [Added]Validating Password has minimum 1 Numeric value

---
+++ UC7Password.py
@@ -0,0 +1,17 @@
+# As a User need to follow pre -defined Password rules.
+#Rule3 – Password must contain one numeriic Value
+import re
+def validate():
+    num=input("Enter Password : ")
+    string="Password is valid : <<password>>"
+    #pattern="^[A-Za-z0-9]{8,}$"
+    #pattern="^(?=.*[A-Z])+[0-9a-zA-Z]{7,}$"
+    pattern="^(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{8,}$"
+    if(re.match(pattern,num)):
+        strg=re.sub("<<password>>",num,string)
+        return strg
+    else:
+        return "Password must contain one numeric Value"
+
+
+print(validate())
